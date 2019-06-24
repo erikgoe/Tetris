52551f0e19a2f96f09708821f27b045c66d023fa
erikgoe <erikgoe@users.noreply.github.com>
2019-06-24 10:30:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tetris
+A Tetris app for Android written with SFML
+
+## Dependencies
+**SFML**
+
+Follow the instructions at https://github.com/SFML/SFML/wiki/Tutorial:-Building-SFML-for-Android to install sfml.
+You will probably need to install Gradle 4.x instead of 5.x.
+
+## Building
+### Linux
+```
+gradle build
+```
---
+++ app/src/main/jni/main.cpp
@@ -0,0 +1,54 @@
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Network.hpp"
+#include "math.h"
+#include <cmath>
+
+int main( int argc, char* argv[] ) {
+    sf::VideoMode screen( sf::VideoMode::getDesktopMode() );
+
+    sf::RenderWindow window( screen, "", sf::Style::Fullscreen );
+    window.setFramerateLimit( 30 );
+
+    sf::View view = window.getDefaultView();
+
+    bool active = true;
+
+    while ( window.isOpen() ) {
+        sf::Event event;
+
+        while ( active ? window.pollEvent( event ) : window.waitEvent( event ) ) {
+            switch ( event.type ) {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::KeyPressed:
+                if ( event.key.code == sf::Keyboard::Escape )
+                    window.close();
+                break;
+            case sf::Event::Resized:
+                view.setSize( event.size.width, event.size.height );
+                view.setCenter( event.size.width / 2, event.size.height / 2 );
+                window.setView( view );
+                break;
+                
+            case sf::Event::MouseLeft:
+                active = false;
+                break;
+            case sf::Event::MouseEntered:
+                active = true;
+                break;
+            }
+        }
+
+        if ( active ) {
+            window.clear( sf::Color::Cyan );
+            
+            window.display();
+        } else {
+            sf::sleep( sf::milliseconds( 100 ) );
+        }
+    }
+}
